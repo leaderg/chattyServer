38232f9bd3ab2f808b6e45ba69283664b1d8cce6
leaderg <leader.g@gmail.com>
2019-06-05 21:14:15 +0000

    Updated README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # chattyServer
+
+## Two part repo
+
+Please access the client side repo at https://github.com/leaderg/chattyApp
---
+++ server.js
@@ -0,0 +1,37 @@
+// server.js
+
+const express = require('express');
+const SocketServer = require('ws').Server;
+const uuidv4 = require('uuid/v4');
+
+//Set up client object.
+const CLIENTS = []
+
+// Set the port to 3001
+const PORT = 3001;
+
+// Create a new express server
+const server = express()
+   // Make the express server serve static assets (html, javascript, css) from the /public folder
+  .use(express.static('public'))
+  .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
+
+// Create the WebSockets server
+const wss = new SocketServer({ server });
+
+// Set up a callback that will run when a client connects to the server
+// When a client connects they are assigned a socket, represented by
+// the ws parameter in the callback.
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('message', event => {
+    let message = JSON.parse(event)
+    message.id = uuidv4();
+    console.dir(message);
+    wss.clients.forEach(client => {
+        client.send(JSON.stringify(message));
+    });
+  })
+  // Set up a callback for when a client closes the socket. This usually means they closed their browser.
+  ws.on('close', () => console.log('Client disconnected'));
+});
